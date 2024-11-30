d7fb7d2fc7f583b6d19ff0fa1b6105ec73a965bb
contrazap <contrazap@gmail.com>
2024-11-30 13:40:17 +0530

    Added signup page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -8,7 +8,11 @@ export const signup = async (req, res, next) => {
 
   try {
     await newUser.save();
-    res.status(201).json({ message: "User created successfully" });
+    res.status(201).json({
+      success: true,
+      statusCode: 201,
+      message: "User created successfully",
+    });
   } catch (error) {
     next(error);
   }
